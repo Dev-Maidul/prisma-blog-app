1e5254842c44af529fa0d79ca5b1776b05b2f864
Dev-Maidu <maidulislammanik8991@gmail.com>
2025-12-27 23:36:55 +0800

    project setup and server create

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import app from "./app";
+import { prisma } from "./lib/prisma";
+
+const PORT=process.env.PORT || 3000;
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("Connected to database successfully ");
+
+    app.listen(PORT,()=>{
+        console.log(`Server is running on ${PORT}`)
+    })
+  } catch (error) {
+    console.log("An error occurs", error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+}
+
+main();
