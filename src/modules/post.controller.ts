33db20aad8fc76fdd1e4658347aef4326126fb2b
Dev-Maidu <maidulislammanik8991@gmail.com>
2026-01-02 00:06:45 +0800

    implement all advance sorting

--- src/modules/post.controller.ts
+++ src/modules/post.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { postService } from "./post.service";
+import { PostStatus } from "../../generated/prisma/enums";
+
 
 const createPost = async (req: Request, res: Response) => {
     try {
@@ -19,19 +21,34 @@ const createPost = async (req: Request, res: Response) => {
     }
 }
 
-const getAllPost=async(req:Request,res:Response)=>{
+
+const getAllPost = async (req: Request, res: Response) => {
     try {
-        const {search}=req.query;
-        const searchString= typeof search==='string' ? search :undefined
-        const allPost=await postService.getAllPost({search:searchString});
-        res.status(200).json({
-            message:"Retrive all post successfully",
-            data:allPost
-        })
-    } catch (error) {
+        const { search } = req.query
+        const searchString = typeof search === 'string' ? search : undefined
+
+        const tags = req.query.tags ? (req.query.tags as string).split(",") : [];
+
+
+        // true or false
+        const isFeatured = req.query.isFeatured
+            ? req.query.isFeatured === 'true'
+                ? true
+                : req.query.isFeatured === 'false'
+                    ? false
+                    : undefined
+            : undefined
+
+        const status = req.query.status as PostStatus | undefined
+
+        const authorId = req.query.authorId as string | undefined
+
+        const result = await postService.getAllPost({ search: searchString, tags, isFeatured, status, authorId })
+        res.status(200).json(result)
+    } catch (e) {
         res.status(400).json({
-            message:"Can not find post",
-            details:error
+            error: "Post creation failed",
+            details: e
         })
     }
 }
