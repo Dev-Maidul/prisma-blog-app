7e678c8a57dfb1a5e1237c4ecc8211086b88daec
Dev-Maidu <maidulislammanik8991@gmail.com>
2026-01-04 21:21:27 +0800

    start comment api

--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 import { PostController } from "./post.controller";
-import auth, { UserRole } from "../middlewares/auth";
+import auth, { UserRole } from "../../middlewares/auth";
+
 
 
 const router = express.Router();
